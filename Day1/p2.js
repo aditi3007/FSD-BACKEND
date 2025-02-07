b0112be427c0cadceb6c79040a3385437ae93aa5
aditi3007 <aditirdc@gmail.com>
2025-02-07 09:39:46 +0530

    fs-module

---
+++ Day1/p2.js
@@ -0,0 +1,6 @@
+const fs =require('fs');
+//Read JSON file
+const newdata = "I am new data."
+// const data = fs.readFileSync('./data.txt','utf8');
+fs.writeFileSync('./data.txt',newdata)
+// console.log(data);
