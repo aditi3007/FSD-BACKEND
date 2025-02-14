953785693ca191f4246b91288fb60e6f2ad4de1a
aditi3007 <aditirdc@gmail.com>
2025-02-14 15:45:28 +0530

    Day3

---
+++ Day3/p1.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req,res)=>{
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello World!');
+});
+//http://localhost:9000/
+server.listen(9000, (err)=>{
+    if(err)
+        console.error(err);
+    else
+    console.log('Server is running on port 9000');
+});
